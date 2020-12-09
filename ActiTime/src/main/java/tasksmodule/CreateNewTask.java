4b849d5735ca147f813c9b0e40541cf03eb84532
Administrator <Administrator@DESKTOP-8CM7VID>
2020-12-09 12:42:52 +0530

    Adding RetryAnalyzer

--- ActiTime/src/main/java/tasksmodule/CreateNewTask.java
+++ ActiTime/src/main/java/tasksmodule/CreateNewTask.java
@@ -6,33 +6,33 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.actitime.genericlib.BaseClass;
+import com.actitime.genericlib.RetryAnalyzer;
 import com.actitime.genericlib.WebdriverCommonLib;
 import com.actitime.objectrepositorylib.CreateNewTasksPage;
 import com.actitime.objectrepositorylib.HomePage;
 import com.actitime.objectrepositorylib.OpenTasksPage;
 
 @Listeners(extentReports.SeleniumListners.class)
-public class CreateNewTask extends BaseClass{
+public class CreateNewTask extends BaseClass {
 
-	@Test(priority=1)
-	public static void createNewTask(){
-		
-		String taskName = "task"+ran.nextInt();
-		
+	@Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
+	public static void createNewTask() {
+
+		String taskName = "task" + ran.nextInt();
+
 		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
 		homePage.tasksLink();
-		
+
 		OpenTasksPage openTasksPage = PageFactory.initElements(driver, OpenTasksPage.class);
 		openTasksPage.createNewTask();
-		
+
 		CreateNewTasksPage createNewTaskPage = PageFactory.initElements(driver, CreateNewTasksPage.class);
-		createNewTaskPage.createNewTask("suri"+ran.nextInt(), "kumar"+ran.nextInt(), taskName);
-		
+		createNewTaskPage.createNewTask("suri" + ran.nextInt(), "kumar" + ran.nextInt(), taskName);
+
 		boolean result = WebdriverCommonLib.verifyLinkIsDisplayed(taskName);
-		
-		
+
 		Assert.assertTrue(result);
-		
+
 	}
-	
+
 }
